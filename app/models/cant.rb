a3cae2ae652846d2373e000d0a5e8e269f1fcb02
Wander Bravo <wander.bravo@gmail.com>
2016-05-19 14:01:01 -0500

    Modify associations and implement to_s method

--- app/models/cant.rb
+++ app/models/cant.rb
@@ -1,3 +1,7 @@
 class Cant < ActiveRecord::Base
   belongs_to :prov
+  has_many :parr
+  def to_s
+    name
+  end
 end
--- app/models/prov.rb
+++ app/models/prov.rb
@@ -1,2 +1,6 @@
 class Prov < ActiveRecord::Base
+  has_many :cants
+  def to_s
+    name
+  end
 end
