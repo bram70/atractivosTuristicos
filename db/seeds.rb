c4ecc6c7393abd9006f5583b97ddaa2b367217eb
Wander Bravo <wander.bravo@gmail.com>
2016-05-24 14:16:45 -0500

    Add seeds for Models

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Prov.create(name: "Guayas")
+Prov.create(name: "Pichincha")
+Prov.create(name: "El Oro")
+
+Cant.create(name: "Guayaquil", prov_id: Prov.find_by(name: "Guayas").id)
+Cant.create(name: "Duran", prov_id: Prov.find_by(name: "Guayas").id)
+Cant.create(name: "Milagro", prov_id: Prov.find_by(name: "Guayas").id)
+
+Cant.create(name: "Quito", prov_id: Prov.find_by(name: "Pichincha").id)
+Cant.create(name: "Mejia", prov_id: Prov.find_by(name: "Pichincha").id)
+Cant.create(name: "Puerto Quito", prov_id: Prov.find_by(name: "Pichincha").id)
+
+
+Parr.create(name: "Ximena", cant_id: Cant.find_by(name: "Guayaquil").id)
+Parr.create(name: "Tarqui", cant_id: Cant.find_by(name: "Guayaquil").id)
+
+Parr.create(name: "Inaquito", cant_id: Cant.find_by(name: "Quito").id)
+Parr.create(name: "Eloy Alfaro", cant_id: Cant.find_by(name: "Quito").id)
+
+Categ.create(name: "Culturales")
+Categ.create(name: "Naturales")
+Tipo.create(name: "Historicos", categ_id: Categ.find_by(name: "Culturales").id)
+Tipo.create(name: "Rios", categ_id: Categ.find_by(name: "Naturales").id)
+
+Subtipo.create(name: "Riachuelos", tipo_id: Tipo.find_by(name: "Rios").id)
+Subtipo.create(name: "Rapidos", tipo_id: Tipo.find_by(name: "Rios").id)
+Subtipo.create(name: "Historia 1", tipo_id: Tipo.find_by(name: "Historicos").id)
+Subtipo.create(name: "Historia 2", tipo_id: Tipo.find_by(name: "Historicos").id)
